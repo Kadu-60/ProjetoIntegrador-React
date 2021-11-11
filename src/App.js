0dbc02fe3c9371f7ffe81a2dd9596aaa2b2eb65d
wilsilmom <wilsilmom@gmail.com>
2021-11-11 12:05:36 -0300

    alteracao de rotas pra versao do router-dom 5.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Routes } from './routes'
+import { BrowserRouter as Router} from 'react-router-dom'
+import { RoutesSite } from './routes'
 import Home from './pages/Home/Home';
 import Busca from './pages/Busca/Busca';
 import Login from './pages/Login/Login';
@@ -8,13 +8,12 @@ import CadastroCliente from './pages/CadastroCliente/CadastroCliente';
 import PedidoFinalizado from './pages/PedidoFinalizado/PedidoFinalizado';
 import Produto from './pages/Produto/Produto';
 import Teste from './pages/Teste';
-import StoreProvider from './components/Context/Provider';
 
 function App(props) {
   return (
     <>
       <Router>
-        <Routes/>
+        <RoutesSite/>
       </Router>
     </>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-
+import StoreProvider from './components/Context/Provider'
+import RoutesPrivate from './components/Routes/Private/Private'
 import Login from './pages/Login/Login'
 import Home from './pages/Home/Home'
-import Categoria from './pages/Categoria/Categoria'
 
-export const Routes = () => {
+
+export const RoutesSite = () => {
     return (
-
-        <Switch>
-            <Route path='/' component={Home} exact/>
-            {/* <Route path="/login" component={Login } /> */}
-            <Route path="/Categoria" component={Categoria} />
-        </Switch>
-
+        <StoreProvider>
+            <Switch>
+                <RoutesPrivate path='/' component={Home} exact/>
+                <Route path="/login" component={Login} />
+            </Switch>
+        </StoreProvider>
     )
 }
