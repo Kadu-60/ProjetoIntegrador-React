4c0d9172915c13294c32198026e70b515273f38b
leandro-ikehara <leandro.ikehara@gmail.com>
2021-11-17 16:33:41 -0300

    Post de cadastro de cliente e Botão Cadastrar Cliente

--- src/pages/CadastroCliente/CadastroCliente.js
+++ src/pages/CadastroCliente/CadastroCliente.js
@@ -1,86 +1,143 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap'
 import './CadastroCliente.css'
 import FormDefault from '../../components/micro/Forms/FormDefault/FormDefault'
-import Input from '../../components/micro/Forms/Input/Input'
+
 import Checkbox from '../../components/micro/Forms/Checkbox/Checkbox'
-import BotaoConfirmar from '../../components/micro/BotaoConfirmar/BotaoConfirmar'
+
+import axios from 'axios'
+import BotaoVoltar from '../../components/micro/BotaoVoltar/BotaoVoltar'
+import BotaoCadastrarCliente from '../../components/micro/BotaoCadastrarCliente/BotaoCadastrarCliente'
 
 function CadastroCliente(props) {
+    const [nome, setNome] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [dataNascimento, setDataNascimento] = useState('')
+    const [email, setEmail] = useState('')
+    const [telefone, setTelefone] = useState('')
+    const [password, setPassword] = useState('')
 
-    return(
+    const Cadastrar = (event) => {
+        event.preventDefault();
+        axios.post("http://localhost:8080/cadastroCliente/salvar", {
+            nome: nome,
+            cpf: cpf,
+            dataNascimento: dataNascimento,
+            email: email,
+            telefone: telefone,
+            password: password
+        })
+            .then((response) => {
+                window.location.href = "http://localhost:3000/"
+            })
+            .catch((error) => console.log(error))
+
+
+    }
+
+    return (
         <>
-        <FormDefault>
-                <br/>    <br/>
+            <FormDefault>
+                <br />    <br />
                 <div class="row d-flex justify-content-center">
-                
+
                     <div class="col d-flex justify-content-center">
-                    <p className="title-dash">Criar conta</p>
+                        <p className="title-dash">Criar conta</p>
                     </div>
-               </div>
-                <br/>
+                </div>
+                <br />
                 <div class="row d-flex justify-content-center">
-                    
+
                     <div class="form-group col-md-6">
-                        <Input corLabel="preto" label="Nome Completo" type="text" required="true"/>
+                        <Form.Label>Nome Completo</Form.Label>
+                        <Form.Control type="nome" placeholder="Ex.: Fulano de Tal" onChange={(event) => {
+                            setNome(event.target.value)
+                        }}
+                            value={nome} required="true" />
                     </div>
-                    
+
                 </div>
-               <div class="row d-flex justify-content-center">
-               
-                    <div class="form-group col-md-3">
-                        <Input corLabel="preto" label="CPF" type="text" placeholder="XXX.XXX.XXX-XX" small="Apenas número" required="true"/>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <Input corLabel="preto" label="Data de nascimento" type="text" placeholder="DD/MM/AAAA" required="true"/>
-                    </div>
-                    
-               </div>
-               <div class="row d-flex justify-content-center">
-             
-                   <div class="form-group col-md-3">
-                       <Input corLabel="preto" label="E-mail" type='text' placeholder="user@email.com" small='Este será seu Login' required="true"/>
-                   </div>
-                   <div class="form-forup col-md-3">
-                       <Input corLabel="preto" label='Telefone' type='text' placeholder="(XX) XXXXX-XXXX" small='Apenas números' required="true"/>
-                   </div>
-               </div>
-               <div class="row">
-                <div class="col-md-3"></div>
-                   <div class="form-group col-md-3">
-                       <Input corLabel='preto' label='Senha' type='password' placeholder="Insira sua senha" small='Deve conter pelo menos 8 caracteres' required="true"/>
-                   </div>
-                    <div class="form-group col-md-3">
-                        <Input corLabel='preto' label='Repita sua senha' type='password' placeholder="Insira sua senha" required="true"/>
-                    </div>
-               </div>
-               
-               <br/><br/>
-               <div class="row justify-content-center">
-                <div class="col-md-6 d-flex justify-content-around">
-                    <div class="form-check col-md-6 termos">
-                        <Checkbox texto="Aceito e concordo com os Termos"/>
-                    </div>
-                </div>
-                </div>
-               <br/>
-               <div class="row d-flex justify-content-center">
-                <div class="col-12 col-md-4  d-flex justify-content-around">
-                    <BotaoConfirmar texto='Criar conta'/>
-                </div>
-             
-             
-                </div>
-               <br/>
-               
                 <div class="row d-flex justify-content-center">
-                
+
+                    <div class="form-group col-md-3">
+                        <Form.Label>CPF</Form.Label>
+                        <Form.Control type="cpf" placeholder="Ex.: 900.000.000-22" onChange={(event) => {
+                            setCpf(event.target.value)
+                        }}
+                            value={cpf} required="true" />
+                    </div>
+                    <div class="form-group col-md-3">
+                        <Form.Label>Nascimento:</Form.Label>
+                        <Form.Control type="dataNascimento" placeholder="Ex.: 29/02/1980" onChange={(event) => {
+                            setDataNascimento(event.target.value)
+                        }}
+                            value={dataNascimento} required="true" />
+                    </div>
+
+                </div>
+                <div class="row d-flex justify-content-center">
+
+                    <div class="form-group col-md-3">
+                        <Form.Label>E-mail:</Form.Label>
+                        <Form.Control type="email" placeholder="Ex.: fu.lano@net.com" onChange={(event) => {
+                            setEmail(event.target.value)
+                        }}
+                            value={email} required="true" />
+                    </div>
+                    <div class="form-forup col-md-3">
+                        <Form.Label>Telefone</Form.Label>
+                        <Form.Control type="telefone" placeholder="Ex.: (11) 98889-7667" onChange={(event) => {
+                            setTelefone(event.target.value)
+                        }}
+                            value={telefone} required="true" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="form-group col-md-3">
+                        <Form.Label>Inserir Senha:</Form.Label>
+                        <Form.Control type="password" placeholder="Ex.: Abc123" onChange={(event) => {
+                            setPassword(event.target.value)
+                        }}
+                            value={password} required="true" />
+                    </div>
+                    <div class="form-group col-md-3">
+                        <Form.Label>Confirmar Senha:</Form.Label>
+                        <Form.Control type="password" placeholder="Ex.: Abc123" onChange={(event) => {
+                            setPassword(event.target.value)
+                        }}
+                            value={password} />
+                    </div>
+                </div>
+
+                <br /><br />
+                <div class="row justify-content-center">
+                    <div class="col-md-6 d-flex justify-content-around">
+                        <div class="form-check col-md-6 termos">
+                            <Checkbox texto="Aceito e concordo com os Termos" />
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div class="row d-flex justify-content-center">
+                    <div class="col-12 col-md-4  d-flex justify-content-around">
+                        <BotaoCadastrarCliente class="conversao" label="Cadastrar" type="submit" onClick={(event) => {
+                            Cadastrar(event)
+                        }} />
+                    </div>
+
+                </div>
+                <br />
+
+                <div class="row d-flex justify-content-center">
+
                     <div class="col-md-4 d-flex justify-content-center">
                         <p>
                             Já possui conta? <a href="../login/login.html"> Fazer login</a>
                         </p>
                     </div>
-               </div>
-               <br/>
+                </div>
+                <br />
             </FormDefault>
         </>
     )
