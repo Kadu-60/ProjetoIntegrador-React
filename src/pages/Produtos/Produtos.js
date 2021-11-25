84c03b20bdaf3b7aa1ba6349bd1066a46c6785e6
Nayara Ferraz <ferrazzznay@gmail.com>
2021-11-25 09:13:25 -0300

    atualizando com a minha página produto e a página produtos

--- src/pages/Produtos/Produtos.js
+++ src/pages/Produtos/Produtos.js
@@ -1,71 +1,97 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import './Produtos.css'
 import ListarProdutos from '../../components/macro/ListarProdutos/ListarProdutos';
 import Produto from '../../components/Produto/Produto';
 import Img1 from '../Home/imgs/ipa-bottle.png'
-import { Icon } from 'semantic-ui-react';
-function Produtos(props){
+import Caneca from '../../assets/imgs/teste/caneca.gif'
+import Colorado from '../../assets/imgs/teste/banner-colorado.png'
+import NovidadesHome from '../../components/macro/NovidadesHome/NovidadesHome';
+import axios from "axios";
+import MarcaItem from '../../components/micro/MarcaItem/MarcaItem';
+
+
+
+
+
+function Produtos(){
+
+    const {id} = useParams()
+    const [ produtos, setProdutos] = useState({})
+   
+    useEffect(() => {
+        
+        axios.get(`http://localhost:8080/Marca/${id}`)
+        .then((response) => {
+           
+            setProdutos(response.data)
+        })
+        .catch((error) => {
+            console.error("Ops! ocorreu um erro"+error)
+        })
+    }, [])
+  
+
+   
 
     return(
-     <>
-            <br/>      <br/>
-        <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="shopping bag"/>Cervejas      <p className="produtos-encontrados">40 produtos</p></p>
+     
+    
+          
+               
+                <body className="products-body" >
+                   
   
+
+                  
+                        <div className="div-global-pag-produtos" >
+                            <MarcaItem produtos = {produtos}/>
+
+                        </div>
                       
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"> <Icon className="icone-cerveja-produtos" name="beer"/>Colorado</p>
- 
-                    
-                   
-                </ListarProdutos>
-                 
-                
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="beer"/>Baden</p>
-                   
-                   
-                </ListarProdutos>
+                        
 
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="beer"/>Hoegaarden</p>
-                   
-                   
-                </ListarProdutos>
+                       
+                                  
+                           
+                            <div className="list-cards-produtos">
+                                <ListarProdutos >
+                                    <NovidadesHome/>
+                                </ListarProdutos>
+                            </div>
+             
+                                
+                                
+                          
+                            <br/>   <br/>
+            
+                        
+        </body>
 
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="beer"/>Patagonia </p>
-                    
-                   
-                </ListarProdutos>
 
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="beer"/>Fritz </p>
-                   
-                   
-                </ListarProdutos>
-
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="beer"/>Mantibier </p>
-                    
-                   
-                </ListarProdutos>
-
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="beer"/>Madalena</p>
-                    
-                   
-                </ListarProdutos>
-
-                <ListarProdutos>
-                <p className="title-dash cervejas-produtos"><Icon className="icone-cerveja-produtos" name="beer"/>Weltenburger</p>
-                    
-                   
-                </ListarProdutos>
-                
-                <br/>   <br/>
-
-     </>
-    )
+                )
+      
+       
+    
+    
 }
 
-export default Produtos
+
+function leiaMais(){
+    var pontos = document.getElementById("pontos");
+    var maisTexto = document.getElementById("mais");
+    var LeiaMais = document.getElementById("LeiaMais");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        LeiaMais.innerHTML="Mais informações";
+    }else {
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        LeiaMais.innerHTML="Recolher"; 
+    }
+}
+
+
+export default Produtos 
