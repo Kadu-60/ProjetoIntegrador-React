eb3d6b015db012485a666b7869272df25cf9b7c0
kadu <carlos.miranda@unesp.br>
2021-12-06 18:41:23 -0300

    forma de pagamento em meus pedidos

--- src/components/Pagamento/Pagamento.js
+++ src/components/Pagamento/Pagamento.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import './Pagamento.css'
 import ImgCartao from "./images/cartao-de-credito.png"
 function Pagamento(props) {
@@ -9,7 +9,7 @@ function Pagamento(props) {
     console.log(props)
     console.log(idPag)
 
-    const metodoDePagamento = (props) =>{
+    const metodoDePagamento = (props) => {
         if (idPag == '1') {
             return (
                 <>
@@ -29,23 +29,58 @@ function Pagamento(props) {
                     </p>
                 </>
             )
-        }else{
+        } else {
             return (
                 <>
+
                     <p class="pagamentoCaixaBoleto d-flex justify-content-center">
-                    À VISTA NO PIX
+                        À VISTA NO PIX
                     </p>
+
+                    
+                    <button type="button" class="btn btnQRCode" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        Ver QR code
+                    </button>
+
+                    
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Seu QR code</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body d-flex flex-column">
+                                <p className='d-flex justify-content-center'>
+                    Seu QR code para pagamento
+                    </p>
+                    <img className='imgPIXFinal' src='https://news.comschool.com.br/wp-content/uploads/2016/05/como-gerar-qrcode-comschool.png' />
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    {/* <p className='textoQRCode'>
+                    Seu QR code para pagamento
+                    </p>
+                    <img className='imgPIXFinal' src='https://news.comschool.com.br/wp-content/uploads/2016/05/como-gerar-qrcode-comschool.png' /> */}
+
                 </>
+
             )
         }
-        
+
     }
 
-    return(
+    return (
         metodoDePagamento()
     )
 
-    
+
 }
 
 export default Pagamento
